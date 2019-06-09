5f0662e20832ceb4c9173ff54c3e2a3fe4530a27
[thisis-Solomon] <[solomonnjobvu1@gmail.com]>
2019-06-09 20:39:32 +0200

    Solutions

--- variables.js
+++ variables.js
@@ -1,1 +1,15 @@
-// you solution should be here
+// your solution should be here
+
+const birthYear = 1994;
+let futureYear = 2021;
+let possibleAge = futureYear - birthYear;
+
+console.log(`${possibleAge} Years`); 
+
+let myAge = 25;
+let maximumAge = 90;
+const amountPerDay = 22.50;
+
+let ageConvertion = (maximumAge - myAge) * 365;
+
+console.log(`You will need ${amountPerDay * ageConvertion} Kwacha to last you until the ripe old age of ${maximumAge}`);
